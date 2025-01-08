7f7e33857ce5bc681c550c52f9781558121309ce
eric <eric.delarosa@cimat.mx>
2025-01-07 19:28:54 -0600

    Elastic Dashboard swerve added

--- src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveDriveSubsystem.java
@@ -1,24 +1,25 @@
 package frc.robot.subsystems;
 
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.networktables.StructArrayPublisher;
+// import edu.wpi.first.networktables.NetworkTableInstance;
+// import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.constants.Constants;
 
 public class SwerveDriveSubsystem extends SubsystemBase {
     // Modules
-    private SwerveModule frontLeftModule;
-    private SwerveModule frontRightModule;
-    private SwerveModule backLeftModule;
-    private SwerveModule backRightModule;
+    private final SwerveModule[] swerveModules;
 
     // Kinematics
     private final double chassisWidth = Units.inchesToMeters(20);
@@ -37,21 +38,22 @@ public class SwerveDriveSubsystem extends SubsystemBase {
     private final Field2d field = new Field2d();
 
     // For debugging purposes
-    private final StructArrayPublisher<SwerveModuleState> currentStatesPublisher;
-    private final StructArrayPublisher<SwerveModuleState> targetStatesPublisher;
-    SwerveModuleState[] currentStates;
-    SwerveModuleState[] targetStates;
+    // private final StructArrayPublisher<SwerveModuleState> currentStatesPublisher;
+    // private final StructArrayPublisher<SwerveModuleState> targetStatesPublisher;
+    // SwerveModuleState[] currentStates;
+    // SwerveModuleState[] targetStates;
 
     public SwerveDriveSubsystem(CommandXboxController driverController) {
-        frontLeftModule = new SwerveModule(1, 2, 16, new Rotation2d());
-        frontRightModule = new SwerveModule(5, 6, 19, new Rotation2d(Math.PI));
-        backLeftModule = new SwerveModule(3, 4, 17, new Rotation2d());
-        backRightModule = new SwerveModule(7, 8, 18, new Rotation2d(Math.PI));
-
+        swerveModules = new SwerveModule[] {
+            new SwerveModule(1,2,16, new Rotation2d()),
+            new SwerveModule(5,6,19, new Rotation2d(Math.PI)),
+            new SwerveModule(3,4,17, new Rotation2d()),
+            new SwerveModule(7,8,18, new Rotation2d(Math.PI))
+        };
         this.driverController = driverController;
 
-        currentStatesPublisher = NetworkTableInstance.getDefault().getStructArrayTopic("/SwerveCurrentStates", SwerveModuleState.struct).publish();
-        targetStatesPublisher = NetworkTableInstance.getDefault().getStructArrayTopic("/SwerveTargetStates", SwerveModuleState.struct).publish();
+        // currentStatesPublisher = NetworkTableInstance.getDefault().getStructArrayTopic("/SwerveCurrentStates", SwerveModuleState.struct).publish();
+        // targetStatesPublisher = NetworkTableInstance.getDefault().getStructArrayTopic("/SwerveTargetStates", SwerveModuleState.struct).publish();
     }
 
     @Override
@@ -59,22 +61,22 @@ public class SwerveDriveSubsystem extends SubsystemBase {
 
         setDesiredStates(getStatesFromSpeeds(getSpeedsFromController()));
         // This method will be called once per scheduler run
-        currentStates = new SwerveModuleState[] {
-            frontLeftModule.getState(),
-            frontRightModule.getState(),
-            backLeftModule.getState(),
-            backRightModule.getState()
-        };
+        // currentStates = new SwerveModuleState[] {
+        //     frontLeftModule.getState(),
+        //     frontRightModule.getState(),
+        //     backLeftModule.getState(),
+        //     backRightModule.getState()
+        // };
 
-        targetStates = new SwerveModuleState[] {
-            frontLeftModule.getTargetState(),
-            frontRightModule.getTargetState(),
-            backLeftModule.getTargetState(),
-            backRightModule.getTargetState()
-        };
+        // targetStates = new SwerveModuleState[] {
+        //     frontLeftModule.getTargetState(),
+        //     frontRightModule.getTargetState(),
+        //     backLeftModule.getTargetState(),
+        //     backRightModule.getTargetState()
+        // };
 
-        currentStatesPublisher.set(currentStates);
-        targetStatesPublisher.set(targetStates);
+        // currentStatesPublisher.set(currentStates);
+        // targetStatesPublisher.set(targetStates);
     }
 
     @Override
@@ -83,10 +85,9 @@ public class SwerveDriveSubsystem extends SubsystemBase {
     }
 
     public void setDesiredStates(SwerveModuleState[] desiredStates) {
-        frontLeftModule.setDesiredState(desiredStates[0]);
-        frontRightModule.setDesiredState(desiredStates[1]);
-        backLeftModule.setDesiredState(desiredStates[2]);
-        backRightModule.setDesiredState(desiredStates[3]);
+        for (int i = 0; i < swerveModules.length; i++) {
+            swerveModules[i].setDesiredState(desiredStates[i]);
+        }
     }
 
     public SwerveModuleState[] getStatesFromSpeeds(ChassisSpeeds speeds) {
@@ -102,55 +103,47 @@ public class SwerveDriveSubsystem extends SubsystemBase {
     }
     @Override
     public void initSendable(SendableBuilder builder) {
-        super.initSendable(builder);
+        builder.setSmartDashboardType("YAGSL Swerve Drive");
         
-        // Set the widget type
-        builder.setSmartDashboardType("SwerveDrive");
+        // Helper method to convert module states to array
+        builder.addDoubleArrayProperty("measuredStates", 
+            () -> getModuleStateArray(true), null);
+            
+        builder.addDoubleArrayProperty("desiredStates",
+            () -> getModuleStateArray(false), null);
 
+        builder.addDoubleProperty("robotRotation",
+            () -> new Rotation2d().getRadians(), null);
         
-        // Front Left Module (0)
-        builder.addDoubleProperty("Front Left Angle", 
-            () -> frontLeftModule.getTargetState().angle.getRadians(), null);
-        builder.addDoubleProperty("Front Left Velocity", 
-            () -> frontLeftModule.getTargetState().speedMetersPerSecond, null);
-        // builder.addDoubleProperty("Front Left Target Angle", 
-        //     () -> frontLeftModule.getTargetState().angle.getRadians(), null);
-        // builder.addDoubleProperty("Front Left Target Velocity", 
-        //     () -> frontLeftModule.getTargetState().speedMetersPerSecond, null);
-
+        builder.addDoubleProperty("maxSpeed",
+            () -> Constants.SwerveDriveConstants.kMaxSpeed, null);
         
-        // Front Right Module (1)
-        builder.addDoubleProperty("Front Right Angle", 
-            () -> frontRightModule.getTargetState().angle.getRadians(), null);
-        builder.addDoubleProperty("Front Right Velocity", 
-            () -> frontRightModule.getTargetState().speedMetersPerSecond, null);
-        // builder.addDoubleProperty("Front Right Target Angle", 
-        //     () -> frontRightModule.getTargetState().angle.getRadians(), null);
-        // builder.addDoubleProperty("Front Right Target Velocity", 
-        //     () -> frontRightModule.getTargetState().speedMetersPerSecond, null);
-
+        // builder.addDoubleProperty("sizeLeftRight",
+        //     () -> Constants.SwerveDriveConstants.kTrackWidth, null);
         
-        // Back Left Module (2)
-        builder.addDoubleProperty("Back Left Angle", 
-            () -> backLeftModule.getTargetState().angle.getRadians(), null);
-        builder.addDoubleProperty("Back Left Velocity", 
-            () -> backLeftModule.getTargetState().speedMetersPerSecond, null);
-        // builder.addDoubleProperty("Back Left Target Angle", 
-        //     () -> backLeftModule.getTargetState().angle.getRadians(), null);
-        // builder.addDoubleProperty("Back Left Target Velocity", 
-        //     () -> backLeftModule.getTargetState().speedMetersPerSecond, null);
+        // builder.addDoubleProperty("sizeFrontBack",
+        //     () -> Constants.SwerveDriveConstants.kWheelBase, null);
+            
+        builder.addStringProperty("rotationUnit",
+            () -> "radians", null);
+    }
 
-        
-        // Back Right Module (3)
-        builder.addDoubleProperty("Back Right Angle", 
-            () -> backRightModule.getTargetState().angle.getRadians(), null);
-        builder.addDoubleProperty("Back Right Velocity", 
-            () -> backRightModule.getTargetState().speedMetersPerSecond, null);
-        // builder.addDoubleProperty("Back Right Target Angle", 
-        //     () -> backRightModule.getTargetState().angle.getRadians(), null);
-        // builder.addDoubleProperty("Back Right Target Velocity", 
-        //     () -> backRightModule.getTargetState().speedMetersPerSecond, null);
-
+    /**
+     * Converts module states to an array format expected by the YAGSL widget
+     * @param current true for current states, false for target states
+     * @return array of alternating angles (rad) and velocities (m/s)
+     */
+    private double[] getModuleStateArray(boolean current) {
+        return Arrays.stream(swerveModules)
+            .flatMap(module -> {
+                SwerveModuleState state = current ? module.getState() : module.getTargetState();
+                return Stream.of(
+                    state.angle.getRadians(),
+                    state.speedMetersPerSecond
+                );
+            })
+            .mapToDouble(Double::valueOf)
+            .toArray();
     }
 
 }
