c12365a1c2d20e1088f7b59bb384aa8062052043
eric <eric.delarosa@cimat.mx>
2025-02-13 23:10:14 -0600

    remove unused code from elevator

--- src/main/java/frc/robot/subsystems/SimpleElevator.java
+++ src/main/java/frc/robot/subsystems/SimpleElevator.java
@@ -1,47 +1,22 @@
 package frc.robot.subsystems;
 
-// import com.revrobotics.spark.SparkMax;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.VictorSPX;
-// import com.revrobotics.spark.SparkBase.PersistMode;
-// import com.revrobotics.spark.SparkBase.ResetMode;
-// import com.revrobotics.spark.SparkLowLevel.MotorType;
-// import com.revrobotics.spark.config.SparkMaxConfig;
-// import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-
 import edu.wpi.first.util.sendable.SendableBuilder;
-// import edu.wpi.first.wpilibj.DigitalInput;
-// import edu.wpi.first.math.filter.Debouncer;
-// import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CoralConstants;
-// import frc.robot.Constants.SimpleElevatorConstants;
 
 public class SimpleElevator extends SubsystemBase {
     // Hardware
     // private final SparkMax motor;
     private final VictorSPX motor;
-    // private final DigitalInput bottomLimitSwitch;
-    // private final DigitalInput topLimitSwitch;
-    // private final Debouncer bottomDebouncer;
-    // private final Debouncer topDebouncer;
 
     // Constants
-    
 
     public SimpleElevator() {
         // Initialize motor
         motor = new VictorSPX(CoralConstants.ARM_MOTOR_ID);
-        // motor = new SparkMax(SimpleElevatorConstants.MOTOR_ID, MotorType.kBrushed);
         configureMotor();
-
-        // Initialize limit switches
-        // bottomLimitSwitch = new DigitalInput(SimpleElevatorConstants.BOTTOM_SWITCH_PORT);
-        // topLimitSwitch = new DigitalInput(SimpleElevatorConstants.TOP_SWITCH_PORT);
-        
-        // // Initialize debouncers in both rising and falling edges
-        // bottomDebouncer = new Debouncer(SimpleElevatorConstants.DEBOUNCE_TIME, DebounceType.kBoth);
-        // topDebouncer = new Debouncer(SimpleElevatorConstants.DEBOUNCE_TIME, DebounceType.kBoth);
     }
 
     private void configureMotor() {
@@ -51,32 +26,14 @@ public class SimpleElevator extends SubsystemBase {
         motor.enableVoltageCompensation(true);
         motor.setNeutralMode(com.ctre.phoenix.motorcontrol.NeutralMode.Brake);
     }
-    // private void configureMotor() {
-    //     SparkMaxConfig config = new SparkMaxConfig();
-    //     config.inverted(true);
-    //     config.smartCurrentLimit(SimpleElevatorConstants.CURRENT_LIMIT);
-    //     config.voltageCompensation(12.0);
-    //     config.idleMode(IdleMode.kBrake);
-    //     motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
-    // }
+
 
     private boolean isSafeToMove(double speed) {
-        // // Get debounced limit switch readings
-        // boolean bottomTriggered = bottomDebouncer.calculate(bottomLimitSwitch.get());
-        // boolean topTriggered = topDebouncer.calculate(topLimitSwitch.get());
-
-        // if (bottomTriggered && speed < 0) {
-        //     return false;
-        // }
-        // if (topTriggered && speed > 0) {
-        //     return false;
-        // }
         return true;
     }
 
     public void setSpeed(double speed) {
         if (isSafeToMove(speed)) {
-            // motor.set(speed);
             motor.set(ControlMode.PercentOutput, speed);
         } else {
             stop();
@@ -84,7 +41,6 @@ public class SimpleElevator extends SubsystemBase {
     }
 
     public void stop() {
-        // motor.set(0);
         motor.set(ControlMode.PercentOutput, 0);
     }
 
